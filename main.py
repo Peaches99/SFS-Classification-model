4e086766d23812be5f4e915970bc34a556df4927
nicolas safarik <nicolas.safarik@gmail.com>
2022-11-23 02:50:51 +0100

    Added prepare

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import tensorflow as tf
-import numpy as np
 import os
-#import the PIL package
+import numpy as np
 import PIL
-
-
+import tensorflow as tf
 
 print("TensorFlow version: {}".format(tf.__version__))
+print("Pillow version: {}".format(PIL.__version__))
 
 #load the images from data/hymenoptera.
 #The images are organized in to a train and validation folder.
@@ -14,7 +12,6 @@ print("TensorFlow version: {}".format(tf.__version__))
 #The ants and bees images are labeled 0 and 1 respectively.
 #The images dont have the same size, so we need to resize them to 500x500.
 #Use any libraries you want to load the images and resize them.
-
 
 def load(path):
     images = []
@@ -33,6 +30,17 @@ def load(path):
                             labels.append(1)
     
     return np.array(images), np.array(labels)
+
+
+def prepare(images, labels):
+    #convert the images to float32
+    images = images.astype('float32')
+    #normalize the images
+    images /= 255
+    #shuffle the images
+    images, labels = tf.keras.utils.shuffle(images, labels)
+    #split the images into train and validation
+    train_images, train_labels = images[:int(len(images)*0.8)], labels[:int(len(labels)*0.8)]
+    val_images, val_labels = images[int(len(images)*0.8):], labels[int(len(labels)*0.8):]
     
-    
-load("data/hymenoptera")
+    return train_images, train_labels, val_images, val_labels
