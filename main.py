5f43c25edd0ed72faf5d626f8f7bba97fd971f74
nicolas safarik <nicolas.safarik@gmail.com>
2023-01-05 15:43:25 +0100

    Version 2 model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import random
 import time
 import PIL
+from matplotlib import pyplot as plt
 import psutil
 import tensorflow as tf
 
@@ -11,7 +12,7 @@ import tensorflow as tf
 from pynvml import nvmlInit
 
 IMAGE_SHAPE = (224, 224, 3)
-EPOCHS = 50
+EPOCHS = 20
 BATCH_SIZE = 32
 LEARNING_RATE = 0.0001
 DATA_DIR = "data/"
@@ -45,39 +46,115 @@ if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
-start_time = time.time()
+def make_dataset():
+    """Create a tf.data.Dataset from the data in the data/ folder."""
+    start_time = time.time()
 
-rand_seed = random.randint(0, 1000)
+    rand_seed = random.randint(0, 1000)
 
-print("Loading images from " + DATA_DIR + " ...")
-dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    DATA_DIR,
-    labels="inferred",
-    label_mode="categorical",
-    class_names=None,
-    color_mode="rgb",
-    batch_size=BATCH_SIZE,
-    image_size=(IMAGE_SHAPE[0], IMAGE_SHAPE[1]),
-    shuffle=True,
-    seed=rand_seed,
-    interpolation="bilinear",
-)
+    print("Loading images from " + DATA_DIR + " ...")
+    dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        DATA_DIR,
+        labels="inferred",
+        label_mode="categorical",
+        class_names=None,
+        color_mode="rgb",
+        batch_size=BATCH_SIZE,
+        image_size=(IMAGE_SHAPE[0], IMAGE_SHAPE[1]),
+        shuffle=True,
+        seed=rand_seed,
+        interpolation="bilinear",
+    )
 
-class_names = dataset.class_names
-print("Class names: " + str(class_names))
+    class_names = dataset.class_names
+    print("Class names: " + str(class_names))
 
-train_size = int(0.8 * len(dataset))
+    train_size = int(0.8 * len(dataset))
 
-train_ds = dataset.take(train_size)
-val_ds = dataset.skip(train_size)
+    train_ds = dataset.take(train_size)
+    val_ds = dataset.skip(train_size)
 
-example_images = val_ds.take(9)
+    print("Training dataset size: " + str(len(train_ds)))
+    print("Validation dataset size: " + str(len(val_ds)))
 
-print("Training dataset size: " + str(len(train_ds)))
-print("Validation dataset size: " + str(len(val_ds)))
+    autotune = tf.data.AUTOTUNE
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=autotune)
+    val_ds = val_ds.cache().prefetch(buffer_size=autotune)
 
-autotune = tf.data.AUTOTUNE
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=autotune)
-val_ds = val_ds.cache().prefetch(buffer_size=autotune)
+    print(
+        "Loading complete after " + str(round(time.time() - start_time, 2)) + " seconds"
+    )
 
-print("Loading complete after " + str(round(time.time() - start_time, 2)) + " seconds")
+    example_images = val_ds.take(9)
+    # show 9 example images and labels
+    # for images, labels in example_images:
+    #     for i in range(9):
+    #         image = images[i].numpy().astype("uint8")
+    #         label = labels[i].numpy()
+    #         label = label.argmax()
+    #         label = class_names[label]
+    #         plt.subplot(3, 3, i + 1)
+    #         plt.xticks([])
+    #         plt.yticks([])
+    #         plt.grid(False)
+    #         plt.imshow(image, cmap=plt.cm.binary)
+    #         plt.xlabel(label)
+
+    # plt.show()
+
+    return train_ds, val_ds, class_names
+
+
+def make_model(class_names):
+    """Create a tf.keras.Model."""
+
+    base_model = tf.keras.applications.VGG19(
+        include_top=False, weights="imagenet", input_shape=IMAGE_SHAPE
+    )
+
+    base_model.trainable = False
+
+    model = tf.keras.Sequential(
+        [
+            base_model,
+            tf.keras.layers.GlobalAveragePooling2D(),
+            tf.keras.layers.Dense(len(class_names), activation="softmax"),
+        ]
+    )
+
+    return model
+
+
+def main():
+    """Main function."""
+    train_ds, val_ds, class_names = make_dataset()
+
+    model = make_model(class_names)
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),
+        loss=tf.keras.losses.CategoricalCrossentropy(),
+        metrics=["accuracy"],
+    )
+
+    model.summary()
+
+    history = model.fit(
+        train_ds,
+        epochs=EPOCHS,
+        validation_data=val_ds,
+    )
+
+    # model.save("model")
+
+    plt.plot(history.history["accuracy"], label="accuracy")
+    plt.plot(history.history["val_accuracy"], label="val_accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.ylim([0.5, 1])
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
