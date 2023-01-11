4c03757b8ce6066dc254aac3fa788303d23a2369
Peaches99 <nicolas.safarik@gmail.com>
2023-01-11 09:09:20 +0100

    improved head

--- src/model.py
+++ src/model.py
@@ -163,13 +163,9 @@ def main():
         [
             base_model,
             tf.keras.layers.GlobalAveragePooling2D(),
-
             tf.keras.layers.Dense(512, activation="relu"),
-            tf.keras.layers.Dropout(0.2),
             tf.keras.layers.Dense(512, activation="relu"),
-            tf.keras.layers.Dropout(0.2),
             tf.keras.layers.Dense(512, activation="relu"),
-            tf.keras.layers.Dropout(0.2),
             tf.keras.layers.Dense(256, activation="softmax"),
             tf.keras.layers.Dropout(0.2),
             tf.keras.layers.Dense(len(class_names), activation="softmax"),
