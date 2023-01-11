95cfe28ac28a27d4af31c923ea5c017bbeb465f4
Peaches99 <nicolas.safarik@gmail.com>
2023-01-11 15:27:31 +0100

    readded larger model

--- src/model.py
+++ src/model.py
@@ -67,17 +67,21 @@ def make_dataset():
     class_names = dataset.class_names
     print("Class names: " + str(class_names))
 
-    # data_augmentation = tf.keras.Sequential(
-    #     [
-    #         tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
-    #         tf.keras.layers.experimental.preprocessing.RandomRotation(0.1),
-    #         tf.keras.layers.experimental.preprocessing.RandomZoom(0.1),
-    #     ]
-    # )
+    data_augmentation = tf.keras.Sequential(
+        [
+            tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
+            tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+            tf.keras.layers.experimental.preprocessing.RandomZoom(0.2),
+            tf.keras.layers.experimental.preprocessing.RandomContrast(0.2),
+            tf.keras.layers.experimental.preprocessing.RandomTranslation(0.2, 0.2),
+            tf.keras.layers.experimental.preprocessing.RandomCrop(
+                IMAGE_SHAPE[0], IMAGE_SHAPE[1]
+            ),
+        ]
+    )
 
-    # dataset = dataset.map(lambda x, y: (data_augmentation(x, training=True), y))
+    dataset = dataset.map(lambda x, y: (data_augmentation(x, training=True), y))
 
-    # split the dataset into train, validation and test
     train_ds = dataset.take(int(len(dataset) * 0.7))
     val_ds = dataset.skip(int(len(dataset) * 0.7))
     val_ds = val_ds.take(int(len(val_ds) * 0.6))
@@ -145,9 +149,12 @@ def main():
     val_ds = val_ds.map(lambda x, y: (preprocess_input(x), y))
     test_ds = test_ds.map(lambda x, y: (preprocess_input(x), y))
 
+<<<<<<< HEAD
     # make a custom callback that saves the best model and replaces it if a better one appears
     # dont actually save the best model as a file but only save it at the end
 
+=======
+>>>>>>> e5bb623 (readded larger model)
     class SaveBestModel(tf.keras.callbacks.Callback):
         def __init__(self):
             self.best_val_acc = 0
@@ -172,8 +179,17 @@ def main():
     model = tf.keras.Sequential(
         [
             base_model,
+<<<<<<< HEAD
             tf.keras.layers.GlobalAveragePooling2D(),
+=======
+            tf.keras.layers.Flatten(),
+>>>>>>> e5bb623 (readded larger model)
             tf.keras.layers.Dense(512, activation="relu"),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(1024, activation="relu"),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(512, activation="relu"),
+            tf.keras.layers.Dropout(0.2),
             tf.keras.layers.Dense(len(class_names), activation="softmax"),
         ]
     )
