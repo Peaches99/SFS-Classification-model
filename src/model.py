b2a4ae85b5988eb42b60faac617bb4c6151d589e
Nicolas Safarik <nicolas.safarik@gmail.com>
2023-01-12 10:45:46 +0100

    Major bug fixes (#6)
    * changes in hyperparameters
    
    * expanding model
    
    * Input preprocessing
    
    * simpler model with higher performance
    
    * lower fine tuning generations
    
    * Custom callback
    
    * Adds convolution head
    
    * expanded conv layer
    
    * removes drouputs
    
    * fixed conv2d size
    
    * modified layers
    
    * added layers
    
    * improved head
    
    * updated head again
    
    * improved head
    
    * removed fine tuning
    
    * readded fine tuning
    
    * augments data
    
    * comments out augmentation
    
    * fixes bug
    
    * increased model size
    
    * testing smaller model
    
    * readded larger model
    
    * removes double callback

--- src/model.py
+++ src/model.py
@@ -151,6 +151,7 @@ def main():
     val_ds = val_ds.map(lambda x, y: (preprocess_input(x), y))
     test_ds = test_ds.map(lambda x, y: (preprocess_input(x), y))
 
+
     class SaveBestModel(tf.keras.callbacks.Callback):
         def __init__(self):
             self.best_val_acc = 0
@@ -158,21 +159,6 @@ def main():
 
         def on_epoch_end(self, epoch, logs=None):
             if logs["val_accuracy"] > self.best_val_acc:
-                self.best_val_acc = logs["val_accuracy"]
-                self.best_model = self.model
-
-        def on_train_end(self, logs=None):
-            self.model = self.best_model
-
-    save_best_model = SaveBestModel()
-
-    class SaveBestModel(tf.keras.callbacks.Callback):
-        def __init__(self):
-            self.best_val_acc = 0
-            self.best_model = None
-
-        def on_epoch_end(self, epoch, logs=None):
-            if logs["val_accuracy"] < self.best_val_acc:
                 self.best_val_acc = logs["val_accuracy"]
                 self.best_model = self.model
 
@@ -190,7 +176,6 @@ def main():
     model = tf.keras.Sequential(
         [
             base_model,
-
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(512, activation="relu"),
             tf.keras.layers.Dropout(0.2),
@@ -218,7 +203,6 @@ def main():
         callbacks=[save_best_model],
     )
 
-
     model = save_best_model.best_model
 
     # train a second time with the base model trainable
@@ -242,7 +226,6 @@ def main():
     evaluated = model.evaluate(test_ds, verbose=2)
     test_acc = evaluated[1]
     print("\nTest accuracy:", test_acc)
-
 
     model.save("./models/" + "sfs_model_" + str((round(test_acc, 4)) * 100) + ".h5")
 
