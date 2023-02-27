9f4ea37f144afd41fa2e787a734a214c9a5ea913
Peaches <nicolas.safarik@gmail.com>
2023-02-27 10:15:03 +0100

    fixed redis docker

--- api/main.py
+++ api/main.py
@@ -3,8 +3,7 @@ from fastapi import FastAPI, File
 import service
 import redis
 
-r = redis.Redis(host="0.0.0.0", port="6379", password="jy972yhkry781rq687yir26")
-
+r = redis.Redis(host='redis', port="6379", password="jy972yhkry781rq687yir26")
 app = FastAPI()
 
 
@@ -13,13 +12,6 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/set/{a}/{b}")
-async def set(a: int, b: int):
-    r.set(a, b)
-
-@app.get("/list")
-async def listAll():
-    return r.keys()
-
 if __name__ == "__main__":
+    r.ping()
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
